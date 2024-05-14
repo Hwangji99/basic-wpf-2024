263f49cf7f8ac3dc71328873cc00fcc02bdda183
Hwangji99 <ghkdwl1303@naver.com>
2024-05-14 17:16:49 +0900

    미니 프로젝트 수정

--- day08/wpf08_project_app/Project_app/MainWindow.xaml.cs
+++ day08/wpf08_project_app/Project_app/MainWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace Project_app
 
         private async void BtnReqRealtime_Click(object sender, RoutedEventArgs e)
         {
-            string openApiUri = "http://apis.data.go.kr/1262000/TravelAlarmService2/getTravelAlarmList2?serviceKey=z6BhxEUBu1diXG%2FWmiJHqqj5SbvVqzr%2BikJSCKelgBVoiUaOonc3nsfQn5S9bQAfr9NIkJSf5qPojF%2BPYaR8qg%3D%3D";
+            string openApiUri = "http://apis.data.go.kr/6260000/BusanPetAnimalInfoService/getPetAnimalInfo?serviceKey=z6BhxEUBu1diXG%2FWmiJHqqj5SbvVqzr%2BikJSCKelgBVoiUaOonc3nsfQn5S9bQAfr9NIkJSf5qPojF%2BPYaR8qg%3D%3D&numOfRows=10&pageNo=1&resultType=json";
             string result = string.Empty;
 
             // WebRequest, WebResponse 객체
@@ -55,36 +55,35 @@ namespace Project_app
                 await this.ShowMessageAsync("오류", $"OpenAPI 조회오류 {ex.Message}");
             }
             var jsonResult = JObject.Parse(result);
-            var status = Convert.ToInt32(jsonResult["currentCount"]);
+            var resultCode = Convert.ToInt32(jsonResult["getPetAnimalInfo"]["header"]["resultCode"]);
 
-            if (status > 0)
+            if (resultCode == 00)
             {
-                var data = jsonResult["data"];
+                var data = jsonResult["getPetAnimalInfo"]["body"]["items"]["item"];
                 var jsonArray = data as JArray; // json자체에서 []안에 들어간 배열데이터만 Array 변환가능
 
-                var travelalarmservice = new List<TravelAlarmService>();
+                var animalRescues = new List<AnimalRescue>();
                 foreach (var item in jsonArray)
                 {
-                    travelalarmservice.Add(new TravelAlarmService()
+                    animalRescues.Add(new AnimalRescue()
                     {
-                        alarm_lvl = Convert.ToInt32(item["alarm_lvl"]),
-                        continent_cd = Convert.ToString(item["continent_cd"]),
-                        continent_eng_nm = Convert.ToString(item["continent_eng_nm"]),
-                        continent_nm = Convert.ToString(item["continent_nm"]),
-                        country_eng_nm = Convert.ToString(item["country_eng_nm"]),
-                        country_iso_alp2 = Convert.ToString(item["country_iso_alp2"]),
-                        country_nm = Convert.ToString(item["country_nm"]),
-                        dang_map_download_url = Convert.ToString(item["dang_map_download_url"]),
-                        flag_download_url = Convert.ToString(item["flag_download_url"]),
-                        map_download_url = Convert.ToString(item["map_download_url"]),
-                        region_ty = Convert.ToString(item["region_ty"]),
-                        remark = Convert.ToString(item["remark"]),
-                        written_dt = Convert.ToDateTime(item["written_dt"])
+                        Ty3Ingye = Convert.ToString(item["ty3Ingye"]),
+                        WritngDe = Convert.ToDateTime(item["writngDe"]),
+                        Ty3Place = Convert.ToString(item["ty3Place"]),
+                        Ty3Sex = Convert.ToString(item["ty3Sex"]),
+                        Cn = Convert.ToString(item["cn"]),
+                        Ty3Date = Convert.ToString(item["ty3Date"]),
+                        Wrter = Convert.ToString(item["wrter"]),
+                        Ty3Insu = Convert.ToString(item["ty3Insu"]),
+                        Ty3Picture = Convert.ToString(item["ty3Picture"]),
+                        Ty3Kind = Convert.ToString(item["ty3Kind"]),
+                        Sj = Convert.ToString(item["sj"]),
+                        Ty3Process = Convert.ToString(item["ty3Process"]),
                     });
                 }
 
-                this.DataContext = travelalarmservice;
-                StsResult.Content = $"OpenAPI {travelalarmservice.Count}건 조회완료!";
+                this.DataContext = animalRescues;
+                StsResult.Content = $"OpenAPI {animalRescues.Count}건 조회완료!";
             }
         }
 
---
+++ day08/wpf08_project_app/Project_app/Models/AnimalRescue.cs
@@ -0,0 +1,94 @@
+﻿using ControlzEx.Theming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_app.Models
+{
+    public class AnimalRescue
+    {
+        public string ResultCode { get; set; } // 결과코드
+        public string ResultMsg { get; set; } // 결과 메시지
+        public int NumOfRows { get; set; } // 한 페이지 결과 수
+        public int PageNo { get; set; } // 페이지 번호
+        public int TotalCount { get; set; } // 전체 결과 수
+        public int NttNo { get; set; } // NTT_NO
+        public string Sj { get; set; } // 구조정보
+        public string Wrter { get; set; } // 작성자
+        public DateTime WritngDe { get; set; } // 작성일
+        public string Cn { get; set; } // 내용
+        public string Ty3Date { get; set; } // 포획일시
+        public string Ty3Place { get; set; } // 포획장소
+        public string Ty3Kind { get; set; } // 동물종류
+        public string Ty3Sex { get; set; } // 성별
+        public string Ty3Process { get; set; } // 처리현황
+        public string Ty3Ingye { get; set; } // 인계
+        public string Ty3Insu {  get; set; } // 인수
+        public string Ty3Picture { get; set; } // 동물사진
+
+        public static readonly string INSERT_QUERY = @"INSERT INTO [dbo].[AnimalRescue]
+                                                                       ([resultCode]
+                                                                       ,[resultMsg]
+                                                                       ,[numOfRows]
+                                                                       ,[pageNo]
+                                                                       ,[totalCount]
+                                                                       ,[nttNo]
+                                                                       ,[sj]
+                                                                       ,[wrter]
+                                                                       ,[writngDe]
+                                                                       ,[cn]
+                                                                       ,[ty3Date]
+                                                                       ,[ty3Place]
+                                                                       ,[ty3Kind]
+                                                                       ,[ty3Sex]
+                                                                       ,[ty3Process]
+                                                                       ,[ty3Ingye]
+                                                                       ,[ty3Insu]
+                                                                       ,[ty3Picture])
+                                                                 VALUES
+                                                                       (@resultCode
+                                                                       ,@resultMsg
+                                                                       ,@numOfRows
+                                                                       ,@pageNo
+                                                                       ,@totalCount
+                                                                       ,@nttNo
+                                                                       ,@sj
+                                                                       ,@wrter
+                                                                       ,@writngDe
+                                                                       ,@cn
+                                                                       ,@ty3Date
+                                                                       ,@ty3Place
+                                                                       ,@ty3Kind
+                                                                       ,@ty3Sex
+                                                                       ,@ty3Process
+                                                                       ,@ty3Ingye
+                                                                       ,@ty3Insu
+                                                                       ,@ty3Picture)";
+
+        public static readonly string SELECT_QUERY = @"SELECT [resultCode]
+                                                             ,[resultMsg]
+                                                             ,[numOfRows]
+                                                             ,[pageNo]
+                                                             ,[totalCount]
+                                                             ,[nttNo]
+                                                             ,[sj]
+                                                             ,[wrter]
+                                                             ,[writngDe]
+                                                             ,[cn]
+                                                             ,[ty3Date]
+                                                             ,[ty3Place]
+                                                             ,[ty3Kind]
+                                                             ,[ty3Sex]
+                                                             ,[ty3Process]
+                                                             ,[ty3Ingye]
+                                                             ,[ty3Insu]
+                                                             ,[ty3Picture]
+                                                         FROM [dbo].[AnimalRescue]";
+
+        //public static readonly string GETDATE_QUERY = @"SELECT CONVERT(CHAR(10), Written, 23) AS Save_Date
+        //                                                  FROM [dbo].[TravelAlarmService]
+        //                                                 GROUP BY CONVERT(CHAR(10), Written, 23)";
+    }
+}
